d6c1f6ae624c83a44b5778a3e26f43b884680d9d
Dan <daniel.gomme@yahoo.co.uk>
2022-08-05 11:53:21 +0100

    Bit of a refactor, start on columns-tagging
    Functions for asking for indices were refactored out, as well as just
    asking for variables and doing things with them.
    
    Variables are now mutable cos that makes life a LOT easier in this
    stage; I don't have to juggle the index AND value AND array of each
    variable. If I need to make the struct immutable again, I'll likely
    make a mutable struct that matches it just to allow for the editing
    on the fly that I'm doing

--- src/variables.jl
+++ src/variables.jl
@@ -1,15 +1,72 @@
 using DataFrames, DataFramesMeta, OrderedCollections, JSON
+import Base: ==
 export Variable
 
-struct Variable
+mutable struct Variable
     name::String
     independent::Bool
 end
+# Here mostly for purposes of testing atm. I'd LIKE for the struct to be immutable, but we're literally changing variable content possibly over and over before confirming.
+# So it's MUCH less fragile to just... not have to juggle indices and variables ALL the time.
+function ==(a::Variable, b::Variable)
+    a.name == b.name && a.independent == b.independent
+end
+
 # Values used to colour dependent and independent variables
 const independentColour = :yellow
 const dependentColour = :light_blue
+
 function getVarColor(var)
     if var.independent independentColour else dependentColour end
+end
+
+function printVariables(variables)
+    for (i, var) in enumerate(variables)
+        printstyled("$i: ", color=getVarColor(var))
+        println(var.name)
+    end
+end
+
+function chooseIndices(array; inputStream = stdin)
+    input = readline(inputStream)
+    if input == ""
+        return missing
+    end
+    elements = split(input, " ")
+    while true
+        try
+            numbers = map(x -> parse(Int, x), elements)
+            if !all(i -> i > 0 && i <= lastindex(array), numbers)
+                error("User supplied number outside bounds of array")
+            end
+            return numbers
+        catch
+            println("Please only write numbers")
+            continue
+        end
+    end 
+end
+
+"""
+`chooseVariablesAndDo(doWithChosen, variables, preMessageFunction)`
+
+Runs preMessageFunction before letting the user choose variables, then running the given function on them
+"""
+# messageFunction lets you do fancy printing without this function ever touching the formatting. But, do a version that lets in just the string?
+function chooseVariablesAndDo(doWithChosen, preMessageFunction, variables; inputStream=stdin, justOnce=false)
+    while true
+        preMessageFunction()
+        printVariables(variables)
+        numbers = chooseIndices(variables; inputStream=inputStream)
+        if ismissing(numbers)
+            break
+        end
+        for num in numbers
+            doWithChosen(variables[num])
+        end
+        if justOnce return variables end
+    end
+    variables
 end
 
 "Ask the user for a set of variables that are present"
@@ -27,30 +84,11 @@ function askForVariables(inputStream=stdin)::Vector{Variable}
 
     println()
     # Allow the user to toggle dependence of any of the variables they've specified so far
-    while true
+    function printF()
         print("Choose which variables to toggle between "); printstyled("dependent", color=dependentColour); print(" and "); printstyled("independent", color=independentColour); println(" (eg '1 3')")
-        for (i, var) in enumerate(variables)
-            printstyled("$i: ", color=getVarColor(var))
-            println(var.name)
-        end
-        input = readline(inputStream)
-        if input == ""
-            break
-        end
-        elements = split(input, " ")
-        try
-            numbers = map(x -> parse(Int, x), elements)
-            if !all(i -> i > 0 && i <= lastindex(variables), numbers)
-                error("User supplied number outside bounds of variables given")
-            end
-            for num in numbers
-                v = variables[num]
-                variables[num] = Variable(v.name, !v.independent)
-            end
-        catch
-            println("Please only write numbers")
-            continue
-        end
+    end 
+    chooseVariablesAndDo(printF, variables, inputStream=inputStream) do var
+        var.independent = !var.independent
     end
     variables
 end
@@ -61,9 +99,9 @@ function readVariablesFromJSON(path)
     end
 end
 
-function askForVariableValues(variableSet::Set{Variable}, enforceAllSet=true, input=stdin)
+function askForVariableValues(variables, enforceAllSet=true, input=stdin)
     results = Dict()
-    for variableName in variableSet
+    for variableName in variables
         print("$(variableName): ")
         value = readline(input)
         if value == "" 
@@ -78,12 +116,19 @@ function askForVariableValues(variableSet::Set{Variable}, enforceAllSet=true, in
     results
 end
 
-function tagData(data, variableSet::Set)
+function tagData(data, variables::Vector{Variable}; inputStream=stdin)
     colNames = names(data)
+    colMeasures = Dict{String, Variable}()
     for colName in colNames
         column = data[:, colName]
         println("Column $(colName)")
-        vars = askForVariableValues(variableSet)
-
+        # show(column)
+        function printF()
+            println("Which variable does this measure? Empty line to skip.")
+        end
+        chooseVariablesAndDo(printF, variables, justOnce=true) do var
+            push!(colMeasures, (colName => var))
+        end
     end
+    colMeasures
 end

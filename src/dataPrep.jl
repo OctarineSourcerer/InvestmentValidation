98e52fed59521a7f5e89d053b177fd087e02942e
Dan <daniel.gomme@yahoo.co.uk>
2022-08-16 10:54:18 +0100

    Add a couple comments to indicate next paths
    Essentially, we'll need a couple dataframes out of this. One for things
    that are once-per-participant and one for several per participant.

--- src/dataPrep.jl
+++ src/dataPrep.jl
@@ -1,6 +1,9 @@
 using CSV, DataFrames
 
 # TODO: Given variables to observe, give back only needed columns
+# We want to observe ID -> per-participant things
+# And separately, ID, objective, aspect...
+# Why? because those objectives and aspects are multiple times per participant, the others just once per participants
 function readAnnotatedData(path, variableNamesRow, variablesToObserve)
-    CSV.File(path; header=headerRow) |> DataFrame
+    CSV.File(path; header=variableNamesRow) |> DataFrame
 end

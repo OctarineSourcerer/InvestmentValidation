62e620602f25470a4f03791919c06ecb4986698b
Dan <daniel.gomme@yahoo.co.uk>
2022-08-17 12:04:52 +0100

    Some absolute voodoo
    The readAnnotatedData function now returns one of the two dataframes we
    want, namely the following columns:
    ID Objective Tension ...
    
    It does so by considering each separate question as an individual
    variable (I guess two individual unique variables count as one), and
    using DataFrames' `stack` function to turn it long; each row containing
    each measurement of those variable combos in the whole dataframe. Then I
    go through each row and extract the objective and aspect from each, and
    well, there we have it.
    
    A note: If I try to broadcast in the @astable part, everything dies, so
    I've had to do it the old-fashioned way? We'll see if I can fix that up.
    
    It's also a little brute-force, as it runs that objective-aspect regex
    on EACH ROW in that quite-long dataframe.

--- src/dataPrep.jl
+++ src/dataPrep.jl
@@ -1,9 +1,29 @@
-using CSV, DataFrames
+using CSV, DataFrames, DataFramesMeta
 
 # TODO: Given variables to observe, give back only needed columns
 # We want to observe ID -> per-participant things
 # And separately, ID, objective, aspect...
 # Why? because those objectives and aspects are multiple times per participant, the others just once per participants
+# I'm gonna call these Tangled questions
 function readAnnotatedData(path, variableNamesRow, variablesToObserve)
-    CSV.File(path; header=variableNamesRow) |> DataFrame
+    primaryKey = "ParticipantID"
+    singleVars = [primaryKey, "Consent", "Experience", "RitualCritical", "AttackedMages", "AttackedYaltha"]
+    implicitVar = :objective # The *hidden* variable - the one we need to shrink columns and expand rows for
+
+    function objectiveAspectPair(colName::String)
+        result = match(r"Objective\((?'objective'\w+)\), (?'aspect'\w+)", colName)
+        (objective = result[:objective], aspect = result[:aspect])
+    end
+    # Each column with an entangled variable, get that variable. Group ones with the same variable. aka partition entangled column names by their entangled variable
+    df = CSV.File(path; header=variableNamesRow) |> DataFrame
+    objectiveColNames = filter(x -> contains(x, "Objective"), names(df))
+
+    # Stack: variable column will contain which cluster (element of objectiveColNames) is being measured. And the value for each
+    stacked = stack(df, objectiveColNames, [:ParticipantID])
+    @transform! stacked @astable begin 
+        parts = map(objectiveAspectPair, :variable) # Can't broadcast here for some reason. I think dataframesMeta is dying on the sumbol replacement?!
+        :Objective = map(x -> getproperty(x, ^(:objective)), parts)
+        :Aspect = map(x -> getproperty(x, ^(:aspect)), parts)
+    end
+    stacked[:, Not(:variable)] |> dropmissing
 end

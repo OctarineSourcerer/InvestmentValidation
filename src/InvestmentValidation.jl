f23d273478c118a7d1cdbbb318d2086e756f33c0
Dan <daniel.gomme@yahoo.co.uk>
2022-08-22 10:12:16 +0100

    Order resulting untangled observations more readably
    Previous ordering had primary key and value first, but that's not so
    readable for humans. So they now come out with primary key first,
    variables in between, value last.

--- src/InvestmentValidation.jl
+++ src/InvestmentValidation.jl
@@ -4,16 +4,16 @@ include("dataPrep.jl")
 
 export shortcut
 
-function shortcut()
-    data = readAnnotatedData("data/annotatedResponses.csv", 4)
-    untangleData(data, "ParticipantID", singleVars, objectiveHeaders, objectiveAspectPair)
-end
-
 function objectiveAspectPair(dataHeader::String)
     result = match(r"Objective\((?'objective'\w+)\), (?'aspect'\w+)", dataHeader)
     (objective = result[:objective], aspect = result[:aspect])
 end
 objectiveHeaders(x) = contains(x, "Objective")
 singleVars = ["Consent", "Experience", "RitualCritical", "AttackMages", "AttackYaltha"]
+
+function shortcut()
+    data = readAnnotatedData("data/annotatedResponses.csv", 4)
+    untangleData(data, :ParticipantID, singleVars, objectiveHeaders, objectiveAspectPair)
+end
 
 end # module
--- src/dataPrep.jl
+++ src/dataPrep.jl
@@ -13,24 +13,25 @@ Return two dataframes in a tuple: `participantData` contains variables supplied 
 
 # Arguments
 - `data`: The dataframe to untangle.
-- `variableNamesRow::Int`: The row in the data that contains annotations saying which variables are measured.
+- `variableNamesRow::Int`: The row in the data that contains annotations saying which variables are measured - eg("")
 - `primaryKey::String`: Name of variable measured that's unique per response. Usually something like ParticipantID
 - `singleVars::Vector{String}`: Variables that are measured once per response, excluding the primary key. For example consent, prior experience etc. This dictates what is returned in `participantData`.
 - `tangledHeaderFilter`: A function that returns true for header names that include the tangled variable.
 - `untangleHeaderText`: A function that takes a tangled header (string), and returns the values of BOTH variables measured in that question in a named tuple, eg `(:objective="FortFirst", :tensionAspect="OwnPower"`
 """
-function untangleData(data, primaryKey::String, singleVars::Vector{String}, tangledHeaderFilter, untangleHeaderText)
-    singleVars = [primaryKey, singleVars...]
+function untangleData(data, primaryKey::Symbol, participantDataVars::Vector{String}, tangledHeaderFilter, untangleHeaderText)
+    participantDataVars = [String(primaryKey), participantDataVars...]
 
     # Each column with an entangled variable, get that variable. Group ones with the same variable. aka partition entangled column names by their entangled variable
     tangledHeaders = filter(tangledHeaderFilter, names(data))
 
     # Stack: variable column will contain which cluster (element of objectiveColNames) is being measured. And the value for each
-    stacked = stack(data, tangledHeaders, [Symbol(primaryKey)])
+    stacked = stack(data, tangledHeaders, [primaryKey])
 
     @rtransform! stacked $AsTable = untangleHeaderText(:variable)
-    observations = stacked[:, Not(:variable)] |> dropmissing
-    participantData = DataFrame(data)[:, singleVars]
+    # Reorder to have primary key first, value last, for readability
+    observations = select(stacked, primaryKey, Not([primaryKey, :variable, :value]), :value) |> dropmissing
+    participantData = DataFrame(data)[:, participantDataVars]
 
     (participantData = participantData, observations = observations)
 end
